c218903dd4bceaed1acd7acae2668b63ec78c0be
totalpl4n <148323222+totalpl4n@users.noreply.github.com>
2026-02-08 13:52:08 +0700

    Create module-installation.bat

---
+++ module-installation.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This program will install OpenCV and NumPy using Pip. Make sure you've installed Pip before running this program, which can be done with pip-installation.bat.
+pause
+
+pip install opencv-python
+
+pip install numpy
